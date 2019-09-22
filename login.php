ca39c6215b0e9f988f527a94957a1d4cfbc57b12
Aragon-Diego <diego.aragonCursos@gmail.com>
2019-09-22 14:18:00 -0500

    Estilos actualizados

--- login.php
+++ login.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="/styles/login.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles/login.css">
 </head>
 <body>
-    <div class="">
-
+    <div class="LoginDiv">
+        <?php include("logo.php");?>
+        <div class="LoginIzq"></div>
+        <div class="LoginDer"></div>
     </div>
 </body>
 </html>
