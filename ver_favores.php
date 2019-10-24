5167505b058fee38cb3810deb41324184a6b6347
Aragon-Diego <diego.aragonCursos@gmail.com>
2019-10-24 14:16:41 -0500

    los ver ya tienen contenidos

--- ver_favores.php
+++ ver_favores.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="styles/dashbord.css">
     <link rel="stylesheet" type="text/css" href="styles/general.css">
     <link rel="stylesheet" type="text/css" href="styles/iconos.css">
+    <link rel="stylesheet" type="text/css" href="styles/vistas.css">    
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -18,7 +19,21 @@
             <?php require('iconos.php');?>
         </div>
         <div class="detalles">
-            <a></a>
+           <h1>Favores</h1>
+           <br/>
+           <br/>
+           <div class="detalles2">
+                <?php 
+                    for($i=0;$i<20;$i++){
+                        echo('<div class="contenido favor">
+                                <h3>{variable con el titulo}</h3>
+                                <h4 class="fecha">dd/mm/aa</h4>
+                                <p class="desc">{variable con descripcion del evento}</p>
+                                <h4 class="lugar">{variable del lugar del evento}</h4>
+                            </div>');
+                    }
+                ?>
+           </div>
         </div>
     </div>
 </body>
