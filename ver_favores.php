2b232cc07a456e1aaf463806b4e0497d5648e466
kics7 <karen.castanedas@udem.edu>
2019-10-27 21:29:10 -0600

    Ya se visualizan los favores

--- ver_favores.php
+++ ver_favores.php
@@ -11,6 +11,24 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
+<?php
+    //Datos de la base
+    $server = "localhost";
+    $user = "basededatos";
+    $pass ="ABC123";
+    $dbase ="kiosco_intel";
+
+    $conexion = mysqli_connect ($server,$user,$pass,$dbase)
+    or die ("Error de conexión:".mysqli_connect_error());
+    mysqli_set_charset($conexion, "utf8");
+    $consulta= mysqli_query ($conexion,"SELECT * FROM favor")
+    or die ("Error en la consulta:".mysql_error());
+    $nfilas = mysqli_num_rows ($consulta);
+    $sum = 0;
+
+    
+
+?>
     <?php
         require('top.php');
     ?>
@@ -23,16 +41,24 @@
            <br/>
            <br/>
            <div class="detalles2">
-                <?php 
-                    for($i=0;$i<20;$i++){
-                        echo('<div class="contenido favor">
-                                <h3>{variable con el titulo}</h3>
-                                <h4 class="fecha">dd/mm/aa</h4>
-                                <p class="desc">{variable con descripcion del evento}</p>
-                                <h4 class="lugar">{variable del lugar del evento}</h4>
-                            </div>');
-                    }
+           <?php
+                
+                for($i=0;$i<$nfilas;$i++){
+                    echo'<div class="contenido favor">';
+                    $tupla = mysqli_fetch_array ($consulta);
+                    $date = date_create($tupla['fechaINI']);
+                     
+                    echo '<h3>', $tupla['titulo'] ,'<h3>';
+                    echo '<br>';
+                    echo '<h4 class="fecha"> Inicio: ',date_format($date, 'd/m/y'),'</h4>';
+                    echo '<p class="desc">',$tupla['contenido'],'</p>';
+                    echo '<h4 class="lugar">Contacto: ',$tupla['lugar'],'</h4>';
+                    echo '<h4 class="lugar">Categoria: ',$tupla['categoria'],'</h4>';
+                    echo '</div>';
+                } 
+                
                 ?>
+                
            </div>
         </div>
     </div>
