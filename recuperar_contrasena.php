1c6869d7bb609ec425e87128a19610fff6a19d1d
Gladys <gladys.garza@udem.edu>
2019-10-29 15:08:04 -0600

    revision

--- recuperar_contrasena.php
+++ recuperar_contrasena.php
@@ -14,7 +14,36 @@
         <?php include("logo.php");?>
         <div class="Reccuadro1">
             <h1>Recuperar Contraseña</h1>
-            <form action="recuperar_contrasena2.php" method="post">
+            <?php
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $server = "localhost";
+                    $user = "root";
+                    $pass = "";
+                    $dbase ="kiosco_intel";
+                    if($_POST['registro']){
+                        $usuario = $_POST['usuario'];
+                        $nacimiento = $_POST['fecha'];
+                        $conexion = mysqli_connect ($server,$user,$pass,$dbase)
+                        or die ("Error de conexion:".mysqli_connect_error());
+                        mysqli_set_charset($conexion, "utf8");
+                        $consulta = mysqli_query ($conexion,"SELECT * FROM users WHERE usuario='$usuario'")
+                        or die ("Error en la consulta:".mysql_error());
+                        $tupla = mysqli_fetch_array ($consulta);
+                        if(!empty($tupla)){
+                            //if(password_verify($usuario, $tupla['user'])){
+                                //echo "Inicio de sesión exitoso";
+                                
+                                $_SESSION['user'] = $usuarioI;
+                                echo "<script>
+                                window.location.href='http://localhost/ComunaNuevo/ver_calendario.php';
+                                </script>";  
+                            //}
+                        }    
+                    }
+                }
+            ?>
+          
+            <form action="recuperar_contrasena.php" method="post">
                 <label>Nombre</label>
                 <input type="text" name="usuario">
                 <label>Fecha de nacimiento</label>
--- recuperar_contrasena2.php
+++ recuperar_contrasena2.php
@@ -14,7 +14,39 @@
         <?php include("logo.php");?>
         <div class="Reccuadro2">
             <h1>Recuperar Contraseña</h1>
-            <form action="dashboard.php" method="post">
+            <?php
+                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $server = "localhost";
+                    $user = "root";
+                    $pass = "";
+                    $dbase ="kiosco_intel";
+                    if($_POST['registro']){
+                        $contra = $_POST['contra'];
+                        $confContra = $_POST['confContra'];
+                        $contraEncrip = password_hash($contra, PASSWORD_DEFAULT);
+
+                        $conexion = mysqli_connect ($server,$user,$pass,$dbase)
+                        or die ("Error de conexion:".mysqli_connect_error());
+                        mysqli_set_charset($conexion, "utf8");
+                        $consulta = mysqli_query ($conexion,"SELECT * FROM users WHERE usuario='$usuario'")
+                        or die ("Error en la consulta:".mysql_error());
+                        $tupla = mysqli_fetch_array ($consulta);
+                        if(!empty($tupla)){
+                            if(password_verify($nacimiento, $tupla['nacimiento'])){
+                                $_SESSION['user'] = $usuario;
+                                echo "<script>
+                                window.location.href='http://localhost/ComunaNuevo/ver_calendario.php';
+                                </script>";
+                                
+                            }
+                        }
+                    }
+                }
+            ?>
+
+
+
+            <form action="recuperar_contrasena2.php" method="post">
                 <label>Contraseña Nueva</label>
                 <input type="text" name="contra">
                 <label>Confirmar Nueva Contraseña</label>
