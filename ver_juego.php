f19e412d8cfe86aceac4237da245ed6a3949d493
Aragon-Diego <diego.aragonCursos@gmail.com>
2019-11-05 00:32:19 -0600

    listo para jugar

--- ver_juego.php
+++ ver_juego.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles/dashbord.css">
-    <link rel="stylesheet" type="text/css" href="styles/general2.css">
     <link rel="stylesheet" type="text/css" href="styles/iconos.css">
+    <link rel="stylesheet" type="text/css" href="styles/general2.css">
 </head>
 <body>
     <?php
@@ -18,36 +18,34 @@
             <?php require('iconos.php');?>
         </div>
         <div class="detalles">
-           <h1>Puntuaciones</h1>
-           <br/>
-           <br/>
-           <div class="detalles2">
-           <?php
-            $archivo = file('actual.txt');
-            $lineas =   count($archivo);
-            ?>
-            <div class = "tabla">
-           <table style="width:100%">
-                <tr>
-                    <th>Participantes</th>
-                    <th>Puntaje</th> 
-                </tr>
-            <?php
-            $k = 0;
-            echo "<tr>";
-            for($i = 0; $i<$lineas-1; $i+=2){ 
-                echo "<tr>";
-                echo "<td>",$archivo[$i],"</td>";
-                echo "<td>",$archivo[$i+1],"</td>";
-                echo "</tr>";
-            }
-            
-           ?>
-        </table>
-            
-        </div>
-            <input type="submit" name="Jugar" class="loginBtn" value="Jugar!"">
-           </div>
+            <h1 class="titulo">Puntuaciones</h1>
+            <br/>
+            <br/>
+            <div class="detalles2">
+                <?php
+                    $archivo = file('actual.txt');
+                    $lineas =   count($archivo);
+                ?>
+                <div class = "tabla">
+                    <table style="width:100%">
+                        <tr>
+                            <th>Participantes</th>
+                            <th>Puntaje</th> 
+                        </tr>
+                        <?php
+                            $k = 0;
+                            echo "<tr>";
+                            for($i = 0; $i<$lineas-1; $i+=2){ 
+                                echo "<tr>";
+                                echo "<td>",$archivo[$i],"</td>";
+                                echo "<td>",$archivo[$i+1],"</td>";
+                                echo "</tr>";
+                            }     
+                        ?>
+                    </table>  
+                </div>
+                <a class="boton" href="webrun:C:\xampp\htdocs\ComunaNuevo\juego\disparos.exe">JUEGA!</a>
+            </div>
         </div>
     </div>
 </body>
